be0f9231166516d458e9bac23633d335d7916fac
Mariana Castro <marianacastrorc@gmail.com>
2023-06-25 03:38:09 -0300

    Updating project details page

--- src/pages/portfolio/project/[id].tsx
+++ src/pages/portfolio/project/[id].tsx
@@ -1,4 +1,14 @@
-import { ProjectContainer } from '@/styles/pages/project'
+import {
+  DescriptionSection,
+  DetailsSection,
+  ImageHeaderContainer,
+  ImagesPreviewContainer,
+  IntroSection,
+  PreviewSection,
+  ProjectContainer,
+  ProjectInfo,
+  Separator,
+} from '@/styles/pages/project'
 import { useRouter } from 'next/router'
 
 export default function ProjectDetails() {
@@ -8,13 +18,50 @@ export default function ProjectDetails() {
   console.log(id)
   return (
     <ProjectContainer>
-      <img
-        className="imgHeader"
-        src="/assets/coffee_delivery/img_1.png"
-        alt=""
-      />
-      <img src="/assets/coffee_delivery/img_2.png" alt="" />
-      <img src="/assets/coffee_delivery/img_3.png" alt="" />
+      <IntroSection>
+        <ImageHeaderContainer>
+          <img
+            className="imgHeader"
+            src="/assets/coffee_delivery/img_1.png"
+            alt=""
+          />
+        </ImageHeaderContainer>
+        <ProjectInfo>
+          <span></span>
+          <h2>Coffee Delivery</h2>
+          <p>
+            Overall, the project aimed to provide a user-friendly interface for
+            managing the shopping cart of a fictional coffee shop, allowing
+            customers to easily select and track their desired items, as well as
+            providing a convenient way to calculate the total value of their
+            purchase.
+          </p>
+          <DetailsSection>
+            <p>Interaction Design / Front End Development</p>
+            <p>HTML / CSS / JS</p>
+            <button>VISIT WEBSITE</button>
+          </DetailsSection>
+        </ProjectInfo>
+        <Separator />
+      </IntroSection>
+      <DescriptionSection>
+        <h2>Project Background</h2>
+        <p>
+          I had the opportunity to practice working with styled-components,
+          contexts, reducers, immer, routing with React Router DOM, React Hook
+          Form, and hookform/resolvers for form validation using zod.
+          Additionally, I worked with hooks like useDebounce and useMemo. I even
+          created my own useLocation hook using the Geolocation API, in order to
+          get user&apos;s location.
+        </p>
+      </DescriptionSection>
+      <PreviewSection>
+        <h2>Static Previews</h2>
+        <ImagesPreviewContainer>
+          <img src="/assets/coffee_delivery/img_2.png" alt="" />
+          <img src="/assets/coffee_delivery/img_3.png" alt="" />
+        </ImagesPreviewContainer>
+      </PreviewSection>
     </ProjectContainer>
   )
 }
--- src/styles/pages/project.ts
+++ src/styles/pages/project.ts
@@ -15,16 +15,134 @@ export const ProjectContainer = styled('div', {
   '@media (min-width: 980px)': {
     gap: '7rem',
   },
+})
+
+export const ImageHeaderContainer = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  margin: '0 auto',
 
   img: {
-    backgroundColor: '$gray800',
+    backgroundColor: '$gray900',
+    borderRadius: '8px',
     padding: '1.5rem 0',
-    width: '311px',
+    width: 'clamp(240px, 80vw, 40rem)',
     height: 'auto',
   },
+})
 
-  '& .imgHeader': {
-    width: '311px',
+export const IntroSection = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+})
+
+export const ProjectInfo = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  textAlign: 'left',
+  width: '100%',
+
+  h2: {
+    fontSize: '2.2rem',
+    color: '$gray300',
+  },
+
+  '> p': {
+    marginTop: '1.5rem',
+    fontSize: '0.93rem',
+    color: '$gray300',
+    lineHeight: '1.85rem',
+    marginBottom: '3.375rem',
+  },
+
+  span: {
+    width: '100%',
+    height: '1.5px',
+    backgroundColor: '$gray700',
+    margin: '2.5rem 0 1.5rem',
+  },
+})
+
+export const DetailsSection = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+
+  p: {
+    fontSize: '0.85rem',
+    fontWeight: 600,
+    lineHeight: '1.8rem',
+    color: '$cyan300',
+  },
+
+  button: {
+    cursor: 'pointer',
+    marginTop: '2rem',
+    padding: '0.75rem 1.8rem',
+    backgroundColor: 'transparent',
+    border: '2px solid $cyan300',
+    color: '$cyan300',
+    fontFamily: 'Space Mono, monospace',
+    textTransform: 'uppercase',
+    width: '11.1rem',
+
+    '&:hover': {
+      backgroundColor: '$cyan700',
+      color: '$white',
+      border: '2px solid $cyan700',
+      transition: '200ms',
+    },
+  },
+})
+
+export const Separator = styled('span', {
+  width: '100%',
+  height: '1.5px',
+  backgroundColor: '$gray700',
+  margin: '1.5rem 0 0',
+})
+
+export const DescriptionSection = styled('span', {
+  display: 'flex',
+  flexDirection: 'column',
+  textAlign: 'left',
+
+  h2: {
+    fontSize: '1.8rem',
+    fontWeight: 400,
+    color: '$gray300',
+  },
+
+  p: {
+    marginTop: '1.75rem',
+    fontSize: '0.93rem',
+    color: '$gray300',
+    lineHeight: '1.85rem',
+  },
+})
+
+export const PreviewSection = styled('span', {
+  display: 'flex',
+  flexDirection: 'column',
+
+  h2: {
+    fontSize: '1.8rem',
+    fontWeight: 400,
+    color: '$gray300',
+    textAlign: 'left',
+  },
+})
+
+export const ImagesPreviewContainer = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.5rem',
+  marginTop: '2.5rem',
+
+  img: {
+    backgroundColor: '$gray900',
+    width: 'clamp(240px, 80vw, 40rem)',
     height: 'auto',
   },
 })
