5f7c7223fea7f717d3160b57da4f702ac95d38da
Mariana Castro <marianacastrorc@gmail.com>
2023-06-25 17:32:03 -0300

    Finishing project details page

--- src/pages/portfolio/project/[id].tsx
+++ src/pages/portfolio/project/[id].tsx
@@ -1,15 +1,28 @@
 import {
+  ControlBorderSeparator,
+  ControlSeparator,
+  ControlText,
+  ControlsContainer,
+  ControlsSection,
   DescriptionSection,
+  DescriptionText,
   DetailsSection,
   ImageHeaderContainer,
   ImagesPreviewContainer,
   IntroSection,
+  MainSection,
+  NextSection,
+  PrevSection,
   PreviewSection,
   ProjectContainer,
+  ProjectContent,
   ProjectInfo,
   Separator,
+  TextProjectInfo,
+  ToolsSection,
 } from '@/styles/pages/project'
 import { useRouter } from 'next/router'
+import { CaretLeft, CaretRight } from 'phosphor-react'
 
 export default function ProjectDetails() {
   const router = useRouter()
@@ -18,50 +31,128 @@ export default function ProjectDetails() {
   console.log(id)
   return (
     <ProjectContainer>
-      <IntroSection>
-        <ImageHeaderContainer>
-          <img
-            className="imgHeader"
-            src="/assets/coffee_delivery/img_1.png"
-            alt=""
-          />
-        </ImageHeaderContainer>
-        <ProjectInfo>
-          <span></span>
-          <h2>Coffee Delivery</h2>
-          <p>
-            Overall, the project aimed to provide a user-friendly interface for
-            managing the shopping cart of a fictional coffee shop, allowing
-            customers to easily select and track their desired items, as well as
-            providing a convenient way to calculate the total value of their
-            purchase.
-          </p>
-          <DetailsSection>
-            <p>Interaction Design / Front End Development</p>
-            <p>HTML / CSS / JS</p>
-            <button>VISIT WEBSITE</button>
-          </DetailsSection>
-        </ProjectInfo>
-        <Separator />
-      </IntroSection>
-      <DescriptionSection>
-        <h2>Project Background</h2>
-        <p>
-          I had the opportunity to practice working with styled-components,
-          contexts, reducers, immer, routing with React Router DOM, React Hook
-          Form, and hookform/resolvers for form validation using zod.
-          Additionally, I worked with hooks like useDebounce and useMemo. I even
-          created my own useLocation hook using the Geolocation API, in order to
-          get user&apos;s location.
-        </p>
-      </DescriptionSection>
-      <PreviewSection>
-        <h2>Static Previews</h2>
-        <ImagesPreviewContainer>
-          <img src="/assets/coffee_delivery/img_2.png" alt="" />
-          <img src="/assets/coffee_delivery/img_3.png" alt="" />
-        </ImagesPreviewContainer>
-      </PreviewSection>
+      <ImageHeaderContainer>
+        <img
+          className="imgHeader"
+          src="/assets/coffee_delivery/img_1.png"
+          alt=""
+        />
+      </ImageHeaderContainer>
+      <ProjectContent>
+        <IntroSection>
+          <Separator />
+          <ProjectInfo>
+            <TextProjectInfo>
+              <h2>Coffee Delivery</h2>
+              <p>
+                Overall, the project aimed to provide a user-friendly interface
+                for managing the shopping cart of a fictional coffee shop,
+                allowing customers to easily select and track their desired
+                items, as well as providing a convenient way to calculate the
+                total value of their purchase.
+              </p>
+            </TextProjectInfo>
+            <DetailsSection>
+              <p>Interaction Design / Front End Development</p>
+              <p>HTML / CSS / JS</p>
+              <button>VISIT WEBSITE</button>
+            </DetailsSection>
+          </ProjectInfo>
+          <Separator />
+        </IntroSection>
+        <MainSection>
+          <DescriptionSection>
+            <DescriptionText>
+              <h2>Project Background</h2>
+              <p>
+                I had the opportunity to practice working with styled-components,
+                contexts, reducers, immer, routing with React Router DOM, React
+                Hook Form, and hookform/resolvers for form validation using zod.
+                Additionally, I worked with hooks like useDebounce and useMemo. I
+                even created my own useLocation hook using the Geolocation API, in
+                order to get user&apos;s location.
+              </p>
+            </DescriptionText>
+            <ToolsSection>
+              <h2>Build with:</h2>
+              <p>
+                <span> {'>'} </span>
+                Typescript
+              </p>
+              <p>
+                <span> {'>'} </span>
+                Next.Js
+              </p>
+              <p>
+                <span> {'>'} </span>
+                Stripe API
+              </p>
+              <p>
+                <span> {'>'} </span>
+                Stitches
+              </p>
+              <p>
+                <span> {'>'} </span>
+                keen-slider
+              </p>
+              <p>
+                <span> {'>'} </span>
+                font-awesome
+              </p>
+              <p>
+                <span> {'>'} </span>
+                lottie-react
+              </p>
+              <p>
+                <span> {'>'} </span>
+                radix-ui
+              </p>
+              <p>
+                <span> {'>'} </span>
+                geolocation API
+              </p>
+              <p>
+                <span> {'>'} </span>
+                ESLint
+              </p>
+              <p>
+                <span> {'>'} </span>
+                Prettier
+              </p>
+            </ToolsSection>
+          </DescriptionSection>
+          <Separator />
+          <PreviewSection>
+            <h2>Static Previews</h2>
+            <span></span>
+            <ImagesPreviewContainer>
+              <img src="/assets/coffee_delivery/img_2.png" alt="" />
+              <img src="/assets/coffee_delivery/img_3.png" alt="" />
+            </ImagesPreviewContainer>
+          </PreviewSection>
+        </MainSection>
+      </ProjectContent>
+      <ControlsContainer>
+        <ControlBorderSeparator />
+        <ControlsSection>
+          <PrevSection>
+            <CaretLeft size={32} />
+            <ControlText>
+              <h2>Bookmark</h2>
+              <p>Previous Project</p>
+            </ControlText>
+          </PrevSection>
+          <ControlSeparator />
+          <NextSection>
+            <CaretRight size={32} />
+            <ControlText>
+              <h2>Bookmark</h2>
+              <p>Previous Project</p>
+            </ControlText>
+          </NextSection>
+        </ControlsSection>
+        <ControlBorderSeparator />
+      </ControlsContainer>
     </ProjectContainer>
   )
 }
--- src/styles/pages/project.ts
+++ src/styles/pages/project.ts
@@ -6,22 +6,22 @@ export const ProjectContainer = styled('div', {
   alignItems: 'center',
   width: '100%',
   padding: '2rem',
-  gap: '3rem',
 
   '@media (min-width: 768px)': {
     padding: '2.5rem',
   },
+})
 
-  '@media (min-width: 980px)': {
-    gap: '7rem',
-  },
+export const ProjectContent = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
 })
 
 export const ImageHeaderContainer = styled('div', {
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
-  margin: '0 auto',
+  margin: '0 auto 2rem',
 
   img: {
     backgroundColor: '$gray900',
@@ -30,15 +30,52 @@ export const ImageHeaderContainer = styled('div', {
     width: 'clamp(240px, 80vw, 40rem)',
     height: 'auto',
   },
+
+  '@media (min-width: 680px)': {
+    margin: '0 auto 1.5rem',
+
+    img: {
+      width: 'clamp(240px, 80vw, 45rem)',
+    },
+  },
+
+  '@media (min-width: 1200px)': {
+    img: {
+      width: 'clamp(240px, 80vw, 60rem)',
+    },
+  },
 })
 
 export const IntroSection = styled('div', {
   display: 'flex',
   flexDirection: 'column',
   width: '100%',
+
+  '@media (min-width: 680px)': {
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
 })
 
 export const ProjectInfo = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  textAlign: 'left',
+  width: '100%',
+  margin: '3rem 0',
+
+  '@media (min-width: 680px)': {
+    maxWidth: '45rem',
+    margin: '0 auto',
+  },
+
+  '@media (min-width: 1200px)': {
+    minWidth: '60rem',
+    margin: '3rem 0',
+  },
+})
+
+export const TextProjectInfo = styled('div', {
   display: 'flex',
   flexDirection: 'column',
   textAlign: 'left',
@@ -49,7 +86,7 @@ export const ProjectInfo = styled('div', {
     color: '$gray300',
   },
 
-  '> p': {
+  p: {
     marginTop: '1.5rem',
     fontSize: '0.93rem',
     color: '$gray300',
@@ -57,11 +94,33 @@ export const ProjectInfo = styled('div', {
     marginBottom: '3.375rem',
   },
 
-  span: {
-    width: '100%',
-    height: '1.5px',
-    backgroundColor: '$gray700',
-    margin: '2.5rem 0 1.5rem',
+  '@media (min-width: 680px)': {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    maxWidth: '45rem',
+    margin: '3rem auto 0',
+
+    p: {
+      maxWidth: '50%',
+      marginTop: '0.8rem',
+      marginBottom: 0,
+      backgroundColor: '$gray800',
+      padding: '1.5rem',
+      borderRadius: '8px',
+    },
+  },
+
+  '@media (min-width: 1200px)': {
+    minWidth: '60rem',
+    margin: '0 auto',
+
+    h2: {
+      fontSize: '2.4rem',
+    },
+
+    p: {
+      maxWidth: '60%',
+    },
   },
 })
 
@@ -94,16 +153,51 @@ export const DetailsSection = styled('div', {
       transition: '200ms',
     },
   },
+
+  '@media (min-width: 680px)': {
+    marginTop: '-9rem',
+    marginBottom: '2rem',
+  },
+
+  '@media (min-width: 1200px)': {
+    marginTop: '-6rem',
+  },
 })
 
 export const Separator = styled('span', {
   width: '100%',
   height: '1.5px',
   backgroundColor: '$gray700',
-  margin: '1.5rem 0 0',
+
+  '@media (min-width: 680px)': {
+    marginTop: '2.5rem',
+  },
 })
 
-export const DescriptionSection = styled('span', {
+export const MainSection = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+export const DescriptionSection = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  textAlign: 'left',
+  margin: '2.5rem 0',
+
+  '@media (min-width: 680px)': {
+    maxWidth: '45rem',
+  },
+
+  '@media (min-width: 1200px)': {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    maxWidth: '60rem',
+    gap: '4rem',
+  },
+})
+
+export const DescriptionText = styled('div', {
   display: 'flex',
   flexDirection: 'column',
   textAlign: 'left',
@@ -114,17 +208,75 @@ export const DescriptionSection = styled('span', {
     color: '$gray300',
   },
 
-  p: {
+  '> p': {
     marginTop: '1.75rem',
     fontSize: '0.93rem',
     color: '$gray300',
     lineHeight: '1.85rem',
   },
+
+  '@media (min-width: 680px)': {
+    h2: {
+      fontSize: '2rem',
+    },
+
+    '> p': {
+      backgroundColor: '$gray800',
+      padding: '1.5rem',
+      borderRadius: '8px',
+    },
+  },
+
+  '@media (min-width: 1200px)': {
+    width: '50%',
+
+    '> p': {
+      backgroundColor: '$gray800',
+      padding: '1.5rem',
+      borderRadius: '8px',
+    },
+  },
 })
 
-export const PreviewSection = styled('span', {
+export const ToolsSection = styled('div', {
+  display: 'none',
+
+  '@media (min-width: 1200px)': {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '1.5rem',
+    borderRadius: '8px',
+    border: '2px solid $gray700',
+    width: '40%',
+
+    h2: {
+      fontFamily: 'Space Mono, monospace',
+      fontSize: '1.1rem',
+      marginBottom: '0.5rem',
+      fontWeight: 600,
+      color: '$gray300',
+    },
+
+    span: {
+      fontFamily: 'Space Mono, monospace',
+      color: '$gray400',
+      fontWeight: 600,
+    },
+
+    p: {
+      fontFamily: 'Space Mono, monospace',
+      fontSize: '0.95rem',
+      color: '$cyan300',
+      marginTop: '0.6rem',
+    },
+  },
+})
+
+export const PreviewSection = styled('div', {
   display: 'flex',
   flexDirection: 'column',
+  width: '100%',
+  margin: '2.5rem 0',
 
   h2: {
     fontSize: '1.8rem',
@@ -132,17 +284,138 @@ export const PreviewSection = styled('span', {
     color: '$gray300',
     textAlign: 'left',
   },
+
+  '@media (min-width: 680px)': {
+    maxWidth: '45rem',
+  },
+
+  '@media (min-width: 1200px)': {
+    h2: {
+      fontSize: '2rem',
+    },
+
+    maxWidth: '60rem',
+  },
 })
 
 export const ImagesPreviewContainer = styled('div', {
   display: 'flex',
   flexDirection: 'column',
   gap: '1.5rem',
-  marginTop: '2.5rem',
+  margin: '3.5rem auto 0',
 
   img: {
     backgroundColor: '$gray900',
     width: 'clamp(240px, 80vw, 40rem)',
     height: 'auto',
+  },
+
+  '@media (min-width: 680px)': {
+    img: {
+      width: 'clamp(240px, 80vw, 45rem)',
+    },
+  },
+
+  '@media (min-width: 1200px)': {
+    flexDirection: 'row',
+    gap: '1rem',
+    maxWidth: '60rem',
+
+    img: {
+      width: 'clamp(240px, 40vw, 29.5rem)',
+    },
+  },
+})
+
+export const ControlsContainer = styled('div', {
+  marginTop: '3rem',
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+
+  '@media (min-width: 680px)': {
+    width: '60rem',
+  },
+
+  '@media (min-width: 1200px)': {
+    width: '65rem',
+  },
+})
+
+export const ControlBorderSeparator = styled('span', {
+  width: '100%',
+  height: '1.5px',
+  backgroundColor: '$gray700',
+})
+
+export const ControlsSection = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  width: '100%',
+})
+
+export const NextSection = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-end',
+  margin: '1rem 0',
+  textAlign: 'right',
+
+  svg: {
+    marginBottom: '1rem',
+    color: '$gray300',
+  },
+
+  '@media (min-width: 680px)': {
+    flexDirection: 'row-reverse',
+    alignItems: 'center',
+    gap: '1rem',
+  },
+})
+
+export const ControlSeparator = styled('span', {
+  width: '1.5px',
+  height: '142px',
+  backgroundColor: '$gray700',
+  margin: '0 1rem',
+
+  '@media (min-width: 680px)': {
+    height: '110px',
+  },
+})
+
+export const PrevSection = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  margin: '1rem 0',
+
+  svg: {
+    marginBottom: '1rem',
+    color: '$gray300',
+  },
+
+  '@media (min-width: 680px)': {
+    flexDirection: 'row',
+    gap: '1rem',
+    alignItems: 'center',
+  },
+})
+
+export const ControlText = styled('div', {
+  h2: {
+    fontSize: '1.5rem',
+    color: '$gray100',
+  },
+
+  p: {
+    fontSize: '1rem',
+    color: '$gray400',
+  },
+
+  '@media (min-width: 680px)': {
+    h2: {
+      fontSize: '2rem',
+    },
   },
 })
